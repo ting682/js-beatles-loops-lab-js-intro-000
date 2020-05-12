90471cfda1ed83477a5f4c00f8cc7b5c9a32fed3
Ting Chung <tchung682@gmail.com>
2020-05-12 20:45:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    array[i] = musicians[i] + "plays" + instruments[i];
+    array[i] = musicians[i] + " plays " + instruments[i];
   }
   return array;
 }
