a37a56320e067eef1449d54a2554c540c7246359
Ting Chung <tchung682@gmail.com>
2020-05-12 20:46:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   for (var i = 0; i < facts.length; i++) {
-    facts[i] = facts[i] + "!!!";
+    facts[i] = facts[i].concat("!!!");
   }
 }
 function iLoveTheBeatles(num){
