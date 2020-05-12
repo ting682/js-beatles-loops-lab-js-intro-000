3cd57c8284137983df084ae02f19cf6f4d3e12ae
Ting Chung <tchung682@gmail.com>
2020-05-12 20:39:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // add solution here
 var musicians [];
-function theBeatlesPlay(
+function theBeatlesPlay(musicians, instruments){
+  for (var i = 0; i < musicians.length; i++) {
+    musicians[i] = instruments
+  }  
+}
+
+function johnLennonFacts(facts){
+  for (var i = 0; i < facts.length; i++) {
+    facts[i] = facts[i] + "!!!";
+  }
+}
+function iLoveTheBeatles(num){
+  var BeatlesArray [];
+  do {
+    BeatlesArray[num] = "I love the Beatles!"
+  } while (num < 15);
+}
